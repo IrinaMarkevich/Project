7c75c966014ca780ecea669a70a5690256f99d82
IrinaMarkevich <irinkamarkevich583@gmail.com>
2019-12-23 15:26:57 +0300

    23.12

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,26 @@
 import { Controller, Post, Body, Get, Put, Delete,Param} from '@nestjs/common';
 import { UserService } from './user.service';
+import {getManager, getRepository} from "typeorm";
 import { USER } from './user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get(':id')
-  getUsers(@Param() params) {
-    //console.log(1)
-    params = 1
-      return this.userService.getUser(params);
+  @Get(":id")
+  getUser(@Param("id") userId: string) {
+      return getManager().findOne(userId);
+  }
+  @Post()
+  create(@Body() user: USER) {
+      return this.userService.createUser(user);
+  }
+  @Put()
+  update(@Body() user: USER) {
+      return this.userService.updateUser(user);
+  }
+  @Delete(':id')
+  deleteUser(@Param() params) {
+      return this.userService.deleteUser(params.id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,13 +14,23 @@ export class UserService {
    async getUsers(user: USER): Promise<USER[]> {
     return await this.entityManager.find(USER);
    }
-   async getUser(_id: string): Promise<USER[]> {
-   return await this.entityManager.find(USER, { id: _id });
-  }
-  // async getUser(_id: string): Promise<USER[]> {
-  //  return await this.entityManager.find(USER, {
-  //     select: ["name", "password", "email", "gender", "age"],
-  //     where: [{ "id": _id }]
-  //   });
+  //  async getUser(_id: string): Promise<USER[]> {
+  //  return await this.entityManager.find(USER, { id: _id });
   // }
+
+  async getUser(_id: string): Promise<USER[]> {
+   return await this.entityManager.find(USER, {
+      select: ["name", "password", "email", "gender", "age"],
+      where: [{ "id": _id }]
+    });
+  }
+  async createUser(user){
+    return await this.entityManager.create(user);
+  }
+  async updateUser(user) {
+    this.entityManager.save(user)
+  }
+  async deleteUser(id) {
+    this.entityManager.delete(USER, id);
+}
 }
